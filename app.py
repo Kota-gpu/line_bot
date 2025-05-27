e9a0394bbd019bbf601d8338e4513e4fa5180cc5
unknown <susan@lilo.com>
2025-05-27 23:56:41 +0800

    daily fluffy reminder

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def send_daily_message():
 
 # 啟動排程器
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(send_daily_message, 'cron', hour=23, minute=55)
+scheduler.add_job(send_daily_message, 'cron', hour=0, minute=10)
 scheduler.start()
 
 # --- Flask 路由 ---
